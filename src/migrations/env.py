333ed5fb36fd4b84d9a843f05209b769f3307149
Victor Val <victortv7@gmail.com>
2024-03-29 18:04:31 +0800

    Add Alembic migrations and scripts

---
+++ src/migrations/env.py
@@ -0,0 +1,81 @@
+import os
+import sys
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy import pool, create_engine
+
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
+sys.path.append(parent_dir)
+
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file input use.
+config = context.config
+fileConfig(config.config_file_name)
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+from src.app.models import Base
+from src.app.core.config import settings
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline():
+    """Run migrations input 'offline' mode.
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+    Calls to context.execute() here emit the given string to the
+    script output.
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=f"{settings.POSTGRES_SYNC_URL}",
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations input 'online' mode.
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+    """
+    connectable = create_engine(
+        f"{settings.POSTGRES_SYNC_URL}",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
+        with context.begin_transaction():
+            context.run_migrations()
+
+    connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
